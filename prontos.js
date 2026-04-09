d9cd38492cedeecfe0c2f6a62b604ec99a1f3a89
ValterdesSR <interlinearnestle@gmail.com>
2026-04-08 22:00:12 -0300

    Update prontos.js
    Mateus 1 adicionado

--- prontos.js
+++ prontos.js
@@ -1,6 +1,7 @@
 // Lista dos capítulos prontos na pasta.
 // Ao produzir um novo capítulo, adicione o nome do arquivo aqui.
 const prontos = new Set([
+  "G01_mateus_01.html",
   "G04_joao_01.html",
   
 ]);
